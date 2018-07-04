c51405a33e278dad070b3c3a887699fdd4af24cf
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-04 22:24:43 +0000

    TMP36 temperature sensor - primitives. 	modified:   tmp36-aa.ino

--- tmp36-aa/tmp36-aa.ino
+++ tmp36-aa/tmp36-aa.ino
@@ -1,11 +1,16 @@
 // Wed  4 Jul 21:59:51 UTC 2018
 // 4737-a3b-00f- // therm/tmp36-aa
 
+int TMP36_PIN = A0; // temperature sensor TMP36 analog out
+
 void setup() {
     Serial.begin(19200);
 }
  
 void loop() {
+    int sensed_temp = analogRead(TMP36_PIN);
+    Serial.print("Sensed temp - raw - ");
+    Serial.println(sensed_temp);
     delay(3200);
 }
 
