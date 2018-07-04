82a29631e9a6688a23ca10087622506d6461d9d3
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-04 22:33:44 +0000

    Volts - directly read (calculated). 	modified:   tmp36-aa.ino

--- tmp36-aa/tmp36-aa.ino
+++ tmp36-aa/tmp36-aa.ino
@@ -1,6 +1,8 @@
 // Wed  4 Jul 21:59:51 UTC 2018
 // 4737-a3b-00f- // therm/tmp36-aa
 
+#define VCC 3.3 // or 5.0 volts
+
 int TMP36_PIN = A0; // temperature sensor TMP36 analog out
 
 void setup() {
@@ -9,8 +11,11 @@ void setup() {
  
 void loop() {
     int sensed_temp = analogRead(TMP36_PIN);
+    float volts = sensed_temp * VCC;
+    volts /= 1024.0;
     Serial.print("Sensed temp - raw - ");
     Serial.println(sensed_temp);
+    Serial.print(volts); Serial.println(" volts");
     delay(3200);
 }
 
