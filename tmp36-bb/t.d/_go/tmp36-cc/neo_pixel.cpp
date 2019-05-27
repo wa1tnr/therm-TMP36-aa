5f0656ab6cf4e5334b4d91dda037ae360d4b3407
wa1tnr <wa1tnr@users.noreply.github.com>
2019-05-27 06:58:52 +0000

    New Files - copies of existing, slight mods. 	new file:   neo_pixel.cpp 	new file:   neo_pixel.h 	new file:   notes_temp_sensor.txt 	new file:   tmp36-cc.ino On branch testing-aa

---
+++ tmp36-bb/t.d/_go/tmp36-cc/neo_pixel.cpp
@@ -0,0 +1,55 @@
+// SO 27 May 2019: This sketch ran on the Metro M0 Express, at the time.
+
+#include <Adafruit_NeoPixel.h> // REQUIRED: https://github.com/adafruit/Adafruit_NeoPixel
+#include "neo_pixel.h"
+#define PINnope         40 // peculiar to Metro M0 Express.  Feather M0 Express: PIN 8 
+#define PIN             12 // Metro M4 Express - as found wired to 8x NeoPixel strip - May 2019
+#define NUMPIXELSnope    1 // Metro M0 Express
+#define NUMPIXELS        8 // external 8x npx strip - may 2019
+
+// kelloggs
+int red   = 0;
+int green = 0;
+int blue  = 0;
+
+// oldcode may 2019: Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, 40, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+/* void dimPixels(void) {
+    pixels.setBrightness(25); } */
+
+void glow_Dark(void) {
+    pixels.setPixelColor(0, 0, 0, 0);
+    delay(25);
+    pixels.show();
+    delay(50);
+}
+
+void glow_Blue1(void) {
+    red   =  0;
+    green =  0;
+    blue  = 5;
+}
+
+void glow_Red1(void) {
+    red   =  2;
+    green =  0;
+    blue  =  0;
+}
+
+void illuminate(void) {
+    glow_Dark();
+    pixels.setPixelColor(0, red, green, blue);
+    delay(20);
+    pixels.show();
+}
+
+void setup_neoPixel(void) {
+    pixels.begin();
+
+    glow_Red1();
+    glow_Blue1();
+
+    illuminate();
+}
+
---
+++ tmp36-bb/t.d/_go/tmp36-cc/tmp36-cc.ino
@@ -0,0 +1,75 @@
+// Wed  4 Jul 19:52:31 UTC 2018
+
+// 0.73 volts at 73 F
+
+
+#include "neo_pixel.h"
+
+int setpoint = 0;
+
+//TMP36 Pin Variables
+int sensorPin = A0; //the analog pin the TMP36's Vout (sense) pin is connected to
+                        //the resolution is 10 mV / degree centigrade with a
+                        //500 mV offset to allow for negative temperatures
+ 
+/*
+ * setup() - this function runs once when you turn your Arduino on
+ * We initialize the serial connection with the computer
+ */
+void setup()
+{
+  Serial.begin(9600);  //Start the serial connection with the computer
+  
+                       //to view the result open the serial monitor 
+  setup_neoPixel();
+
+#ifdef AVR_MCU
+  analogReference(EXTERNAL);
+#endif
+
+#ifdef ARDUINO_SAMD_ZERO
+  analogReference(AR_EXTERNAL);
+#endif
+}
+ 
+void loop()                     // run over and over again
+{
+ //getting the voltage reading from the temperature sensor
+ int reading = analogRead(sensorPin);  
+ // int reading = 220;
+ 
+#define VCC 3.3
+ // converting that reading to voltage, for 3.3v arduino use 3.3
+ float voltage = reading * VCC;
+ voltage /= 1024.0; 
+ 
+ // print out the voltage
+ // Serial.print(voltage); Serial.println(" volts");
+ 
+ // now print out the temperature
+ float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                               //to degrees ((voltage - 500mV) times 100)
+ // Serial.print(temperatureC); Serial.println(" degrees C");
+ 
+ // now convert to Fahrenheit
+ float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+ // Serial.print(temperatureF); Serial.println(" degrees F");
+ 
+ // setpoint = 55 ; // 20 second response time after cycle OFF air conditioner compressor!
+
+ // may 2019 - went to 5 volts probably 3.3v calib // setpoint = 51 ;
+ setpoint = 51 ;
+
+ if (temperatureF < setpoint) {
+    glow_Blue1(); // cool
+ }
+ if (temperatureF > (setpoint + 3)) {
+    glow_Red1(); // warm
+ }
+
+ illuminate(); // perform the glowance action itself
+
+ // delay(12000);
+ delay(3200);
+
+}
