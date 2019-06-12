169b28f28863f7bb9660a9f84c986fd8d4afe5a1
wa1tnr <wa1tnr@users.noreply.github.com>
2019-06-12 17:28:18 +0000

    Many changes - needs breakdown change by change
    This branch seems to be recording experiments made
    using the pwm function to drive an analog panel
    meter.
    
    Meter is centered on approx. 62F, and should have
    enough range (in this very edit) for all common
    conditions.  Reads about 37/50 (2/3 scale deflection)
    at 73F and bottoms out in the upper 40's or low 50's.
    
    Set point is 52F. A set point of 48F is to be taken
    as difficult to achieve (true or untrue, it's an
    assumption here).
    
    On branch testing-aa-c
    	modified:   tmp36-cc.ino

--- tmp36-cc/tmp36-cc.ino
+++ tmp36-cc/tmp36-cc.ino
@@ -1,3 +1,4 @@
+// Wed Jun 12 17:27:41 UTC 2019
 // Tue Jun 11 21:34:46 UTC 2019
 // Mon Jun 10 02:57:54 UTC 2019
 
@@ -16,6 +17,94 @@
 // Wed  4 Jul 19:52:31 UTC 2018
 
 // filomena
+
+// TODO: hysteresis not yet programmed 12 June 2019.
+//       If it were present, the system would be able
+//       to 'know' if the temperature were recently
+//       rising .. or falling.
+//       Right now, it does not - and that shows.
+//       A classic example of why hysteresis is needed
+//       for this kind of system: presently, the system
+//       keeps reporting all three conditions, when near
+//       the border between three states (on, off, maybe on/off).
+//       What is desired (instead) is a trend towards
+//       becoming warmer (or cooler) and a lock on that
+//       trend.
+//       If it was recently cooler, and is now warming,
+//       then once it reliably reaches the warmer state,
+//       the sytem should report 'it is now warm enough'
+//       by lighting in red (only).
+//       Similarly, blue, if it was recently a good deal
+//       warmer, but has now reached a sufficently cold
+//       state to warrant the announcement 'it is now
+//       cold enough' (wherein it lights only the blue).
+//       The 'maybe' state is a kludge, for purposes of
+//       development - the completed project won't actively
+//       use this (it'll be used, if at all, only to
+//       debug or otherwise further refine things).
+//       A mechanical thermostat (which is the basic
+//       model for the project .. sans 'temperature history'
+//       in the sense of reporting on past events, long
+//       after they are no longer operative in any
+//       meaningful way) .. a mechanical thermostat works
+//       by means of a glass vial with a drop of Mercury
+//       in it.  The mercury has the quality of bunching
+//       up together (high surface tension?) and so it
+//       tends to flow rapidly from one end of the vial
+//       to the other, and does not want to stay anywhere
+//       near the middle of the glass vial.
+//       There is enough mechanical 'amplification' to
+//       (mostly) prevent the in-between state, and at
+//       any rate, the Mercury, in its job as a kind of
+//       SPST switch, can only connect the two electrodes
+//       at (one) far end of the vial.
+//       The vial itself, of course, rides on a bi-metalic
+//       coil, which gets tight or loose, depending on the
+//       ambient temperature; as the two strips of (dissimilar)
+//       metals have different rates of expansion, the coil
+//       that they've been formed into tends to tighten or
+//       to loosen, which translates to a rotation applied
+//       to the Mercury switch (the glass vial) attached
+//       to the outside of it.
+//       So the switch rocks back and forth, as temperature
+//       varies sufficiently to flip the Mercury blob to
+//       one end of the vial (or the other, depending).
+//       It won't oscillate, however - the Mercury blob has
+//       just enough mass to deform the bi-metalic coil,
+//       just a bit, which has the effect of 'amplifying'
+//       the 'signal' (temperature state) to keep the system
+//       from going into (thermal!) (mechanical!) oscillation,
+//       near the set-point (desired temperature).
+//       The system preserves the 'history' of temperature
+//       change, by the weight of the Mercury blob, which
+//       (again) gathers at one end of the glass vial, or
+//       the other -- and does not want to (then) move back
+//       to the opposite end, from small changes in thermal
+//       input.
+//       Thus, a certain amount of temperature change (in
+//       either direction; they do not have to be exactly
+//       equal) is the only way to cause the system to change
+//       to the 'other' state.
+//       So, in effect, the system 'remembers' from whence
+//       it came:
+//       If the switch is ON, that was caused by a falling
+//       temperature regime (most recently), once the system
+//       has reached equillibrium (either a steady temperature;
+//       or a temperature regime that is mostly rising, or
+//       mostly falling -- or if the set-point is changed by
+//       an outside agent (usually, a human).
+//       Conversely, if the switch is presently OFF, that state
+//       also has a historic cause: the ambient temperature
+//       was in a rising regime (the furnace was operating,
+//       raising the temperature in the room the thermostat
+//       is located in) and in this rising regime, a trip
+//       point was reached where the Mercury blob slid over
+//       to the other side of the vial, cutting off the
+//       switch by opening the contact (formerly) made by
+//       the Mercury blob (which itself is a good conductor
+//       of electric current).
+
+
 
 // narrative:  timex does okay to tick tock out 5.7 second intervals;
 //        instead, what is wanted is 5 minute intervals (do not reset
@@ -46,7 +135,9 @@ int sensorPin = A0; //the analog pin the TMP36's Vout (sense) pin is connected t
                         //the resolution is 10 mV / degree centigrade with a
                         //500 mV offset to allow for negative temperatures
  
-int pwmPin = A2; // cannot use A0 nor A1 for PWM.
+int pwmPin = A2;    // cannot use A0 nor A1 for PWM.
+
+uint8_t hourvoltage;
 
 int hysteresis = millis(); // time_monotonic();
 
@@ -55,9 +146,12 @@ int hysteresis = millis(); // time_monotonic();
  * We initialize the serial connection with the computer
  */
 #define TURNKEY_COMPILE
+
+
 void setup()
 {
   Serial.begin(9600);  //Start the serial connection with the computer
+  pinMode(pwmPin, OUTPUT);
   // turnkey - do not wait for serial connection // while(!Serial) { }
 #ifndef TURNKEY_COMPILE
   while(!Serial) { }
@@ -75,7 +169,6 @@ void setup()
 #ifdef ARDUINO_SAMD_ZERO
   analogReference(AR_EXTERNAL);
 #endif
-  // Serial.println("setup() has completed. ");
 }
  
 void color_toggled(void) {
@@ -84,30 +177,19 @@ void color_toggled(void) {
     // no error test for third condition 'other'
 }
 
-// void timex(void) { } // top down // int hyster_time_a = millis(); // time_monotonic();
 void timex(void) {
-    // Serial.println("timex is reached.");
     int new_hysteresis = millis(); // now there is a new reading of the clock to compare to.
     int difference = new_hysteresis - hysteresis ;
 
-    // every 25 seconds, toggle the color of the first NeoPixel in the 8x strip.
-    // if (difference > 24999) {// elapsed time 25 seconds (or larger)
-    // if (difference > (60*5*1000)) {// elapsed time 5 minutes (or larger)
     if (difference > (60*3*1000)) {// elapsed time 3 minutes (or larger)
-
-    // if (difference > (3*6*1000)) {// elapsed time 18 seconds (or larger)
         hysteresis = new_hysteresis; // actual hysteresis record event here
         // copy the most recent reading into the history buffer, overwriting the past record
         // this is done only when the goal of 25 seconds of elapsed time has been reached.
-        // meow.
 
-
-        // these two were swapped.  Want an accurate stack report
         illuminate(); // perform the glowance action itself
         dotS(); // show the stack //
 
 #ifdef TESTING_ONLY
-        // testing only
         color_toggled();
 
         if (color_toggle) {
@@ -118,16 +200,33 @@ void timex(void) {
 #endif
 
     }
-
     Serial.print(" delta hysteresis is  (should be 5695 ms): ");
     Serial.println(difference);
 }
 
+
+void pwm_sender(void) {
+  analogWrite(pwmPin, hourvoltage);
+}
+
+// coefficient
+#define COEFF 0.66
+
+// 0.492  12.1%
+// 0.481   0.00%
+// 0.483
+
 void loop()                     // run over and over again
 {
- //getting the voltage reading from the temperature sensor
- int reading = analogRead(sensorPin);  
- // int reading = 220;
+  uint8_t hourvalue;
+  int reading = analogRead(sensorPin);  // 233 to 264 - hand warming to ambient 72F
+  float intmd_val = 0;
+
+  hourvalue = (100 * reading); // as in 55 deg F
+
+  // map(value, fromLo, fromHi, toLo, toHi);
+  // hourvoltage = map(reading, 218, 274, 21, 234);     // Convert hour to PWM duty cycle
+  // pwm_sender(); // call the sender
  
 #define VCC 3.3
  // converting that reading to voltage, for 3.3v arduino use 3.3
@@ -144,7 +243,7 @@ void loop()                     // run over and over again
  
  // now convert to Fahrenheit
  float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
- // Serial.print(temperatureF); Serial.println(" degrees F");
+ Serial.print(temperatureF); Serial.println(" degrees F");
  
  // setpoint = 55 ; // 20 second response time after cycle OFF air conditioner compressor!
 
@@ -175,6 +274,23 @@ void loop()                     // run over and over again
     show_master_pixel();
  }
 
+
+ // 52.7 * 50 = 2635
+ // 72.4 * 50 = 3620
+ // 83.7 * 50 = 4185
+
+ // override for test:
+ intmd_val = 50 * 48.2; // temp in fahrenheit
+ intmd_val = 50 * 76.7; //
+
+ intmd_val = 50 * 76.7; //
+ intmd_val = 50 * 62.4; //
+
+
+ intmd_val = 50 * temperatureF;
+ // map(value, fromLo, fromHi, toLo, toHi);
+ hourvoltage = map(intmd_val, 2405, 3840, 21, 234);     // Convert hour to PWM duty cycle
+ pwm_sender(); // call the sender and give it hourvoltage
 
  // Serial.println("pre-timex reached - loop iteration");
 
